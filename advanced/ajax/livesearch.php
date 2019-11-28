5d97485455177828406e0911971b499fba9f841b
linut <fenzang6255@sina.com>
2019-11-28 17:30:07 +0800

    Realizing real-time searching data from XML files by AJAX.

--- advanced/ajax/livesearch.php
+++ advanced/ajax/livesearch.php
@@ -15,3 +15,41 @@ $q = $_GET['q'];//get parameter
 
 //如果q参数存在则从xml文件中查找数据
 
+if (strlen($q)>0){
+
+    $hint="";
+
+    for ($i=0;$i<($x->length);$i++){
+        $y = $x->item($i)->getElementsByTagName('title');
+        $z = $x->item($i)->getElementsByTagName('url');
+        if ($y->item(0)->nodeType==1){
+            //找到匹配搜索的链接
+            if (stristr($y->item(0)->childNodes->item(0)->nodeValue,$q)){
+                if ($hint==""){
+                    $hint="<a href='".
+                        $z->item(0)->childNodes->item(0)->nodeValue.
+                        "' target='_blank'>".
+                        $y->item(0)->childNodes->item(0)->nodeValue.
+                        "</a>";
+                }else{
+                    $hint=$hint."<br /><a href='".
+                        $z->item(0)->childNodes->item(0)->nodeValue.
+                        "' target='_blank'>".
+                        $y->item(0)->childNodes->item(0)->nodeValue.
+                        "</a>";
+                }
+            }
+        }
+    }
+}
+
+//如果没找到则返回 "no suggestion"
+if ($hint==""){
+    $response = "no suggestion";
+}else{
+    $response = $hint;
+}
+
+//输出结果
+
+echo $response;
